79e6bb2580c7b63c948906c65e9a948f81137195
Ayush <guptaayush630@gmail.com>
2020-08-31 22:35:19 +0530

    Adding CSS StyleSheets

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var items = ["Buy Momos","Eat Momos","Buy More Momos","Eat More Momos"];
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
     
