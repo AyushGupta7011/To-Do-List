d83ce3d815704945cf3c17decee34077bf528928
Ayush <guptaayush630@gmail.com>
2020-09-03 22:07:38 +0530

    Adding date.js

--- app.js
+++ app.js
@@ -1,50 +1,46 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-var items = ["Buy Momos","Eat Momos","Buy More Momos","Eat More Momos"];
-let workItems = [];
-
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/", function(req,res){
+const items = ["Buy Momos","Eat Momos","Buy More Momos","Eat More Momos"];
+const workItems = [];
 
-    res.render("list", {listTitle: day, newListItems: items});
+app.get("/", function(req, res) {
+
+const day = date.getDate();
+
+  res.render("list", {listTitle: day, newListItems: items});
 
 });
 
 app.post("/", function(req, res){
 
-    let item =req.body.newItem;
+  const item = req.body.newItem;
 
-    if(req.body.list === "Work"){
-      workItems.push(item);
-      res.redirect("/work");
-    }
-    else{
-        items.push(item);
-        res.redirect("/");
-    }
-
-
-});
-app.get("/work", function(req,res){
-    res.render("list",{listTitle: "Work List", newListItems: workItems});
-});
-
-app .post("/work", function(req,res){
-    let item =req.body.newItem;
+  if (req.body.list === "Work") {
     workItems.push(item);
     res.redirect("/work");
-})
-app.get("/about", function(res,req){
-    res.render("about");
-})
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
 
-app.listen(3000, function(){
-    console.log("Server is up");
-})
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
