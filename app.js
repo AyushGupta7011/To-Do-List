3c35e439b59753cede4b0b623662f6d602767a7d
Ayush <guptaayush630@gmail.com>
2020-09-05 22:27:48 +0530

    Adding Mongoose in ToDoList

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose =require("mongoose");
 const date = require(__dirname + "/date.js");
 
 const app = express();
@@ -9,14 +10,39 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Momos","Eat Momos","Buy More Momos","Eat More Momos"];
-const workItems = [];
+mongoose.connect("mongodb://localhost:/27017/todolistDB",{useNewUrlParser: true},{ useUnifiedTopology: true });
+
+const itemsSchema = {
+  name: String
+};
+ 
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name:"Welcome to your todolist!"
+});
+
+const item2 = new Item({
+  name:"Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+  name:"<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, function(err){
+  if(err){
+    console.log(err);
+  } else {
+    console.log("Successfully saved default items to DB.");
+  }
+});
 
 app.get("/", function(req, res) {
 
-const day = date.getDate();
-
-  res.render("list", {listTitle: day, newListItems: items});
+  res.render("list", {listTitle: "Today", newListItems: items});
 
 });
 
