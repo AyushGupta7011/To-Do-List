94575816bde961bda3219a47d4b65544b35c3b67
Ayush <guptaayush630@gmail.com>
2020-09-06 22:52:37 +0530

    Rendering Items in the ToDoList App

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose =require("mongoose");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+//const _ = require("lodash");
 
 const app = express();
 
@@ -10,39 +10,54 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:/27017/todolistDB",{useNewUrlParser: true},{ useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true}, { useUnifiedTopology: true });
 
 const itemsSchema = {
   name: String
 };
- 
+
 const Item = mongoose.model("Item", itemsSchema);
 
+
 const item1 = new Item({
-  name:"Welcome to your todolist!"
+  name: "Welcome to your todolist!"
 });
 
 const item2 = new Item({
-  name:"Hit the + button to add a new item."
+  name: "Hit the + button to add a new item."
 });
 
 const item3 = new Item({
-  name:"<-- Hit this to delete an item."
+  name: "<-- Hit this to delete an item."
 });
 
 const defaultItems = [item1, item2, item3];
 
-Item.insertMany(defaultItems, function(err){
-  if(err){
-    console.log(err);
-  } else {
-    console.log("Successfully saved default items to DB.");
-  }
-});
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
 
 app.get("/", function(req, res) {
 
-  res.render("list", {listTitle: "Today", newListItems: items});
+  Item.find({}, function(err, foundItems){
+
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function(err){
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully savevd default items to DB.");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
+  });
 
 });
 
