ba0fbd7bfac639767b584376abed3a9bb064c09b
Ayush <guptaayush630@gmail.com>
2020-09-08 21:56:54 +0530

    Deleting Items from our ToDoList Database

--- app.js
+++ app.js
@@ -69,9 +69,28 @@ app.post("/", function(req, res){
     name: itemName});
 
     item.save();
-
     res.redirect("/");
 
+});
+
+app.post("/delete", function(req, res){
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+
+  if (listName === "Today") {
+    Item.findByIdAndRemove(checkedItemId, function(err){
+      if (!err) {
+        console.log("Successfully deleted checked item.");
+        res.redirect("/");
+      }
+    });
+  } else {
+    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList){
+      if (!err){
+        res.redirect("/" + listName);
+      }
+    });
+  }
 });
 
 app.get("/work", function(req,res){
