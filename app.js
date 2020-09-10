5973993c650d7f1b0441b068d4f350f3afb819db
Ayush <guptaayush630@gmail.com>
2020-09-10 17:50:17 +0530

    Adding new items to ToDoList

--- app.js
+++ app.js
@@ -64,14 +64,24 @@ app.get("/", function(req, res) {
 app.post("/", function(req, res){
 
   const itemName = req.body.newItem;
+  const listName = req.body.list;
 
   const item = new Item({
-    name: itemName});
+    name: itemName
+  });
 
+  if (listName === "Today"){
     item.save();
     res.redirect("/");
-
+  } else {
+    List.findOne({name: listName}, function(err, foundList){
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
+  }
 });
+
 
 app.post("/delete", function(req, res){
   const checkedItemId = req.body.checkbox;
